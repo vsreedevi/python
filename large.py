60125a18ca02f3e5fe5d8d0cd502c5c25902f6be
vsreedevi <47561673+vsreedevi@users.noreply.github.com>
2019-02-13 09:53:14 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,9 @@
+a=raw_input("Enter the first numner")
+b=raw_input("Enter the second Number")
+c=raw_input("Enter the third number")
+if( a>b and a>c):
+  print("The largest number is ",a)
+elif(b>a and b>c):
+  print("The largest number is ",b)
+else:
+  print("The largest number is ",c)
